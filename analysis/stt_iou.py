9c676efc71e5293b32604a934e79dadea31241a8
box_of_the_fox <ernest.smiech@wp.pl>
2024-02-06 19:01:18 +0100

    poprawki do STT

--- analysis/stt_iou.py
+++ analysis/stt_iou.py
@@ -2,59 +2,28 @@ import json
 
 import pandas
 import streamlit as st
-from Geometry3D import *
 from shapely import Polygon
 
-from analysis.utils import create_polygon, polygon_to_intarray
+from analysis.utils import create_polygon
 from stack import results_file
 
 
-def create_cubes(_tr: list[list[int]]):
-    polygons = []
-    for z, t in enumerate(_tr):
-        points = []
-        for x,y in zip(t[:-2:2], t[1:-2:2]):
-            points.append(Point(x,y,z))
-        polygons.append(points)
+def stt_iou(_tr: list[Polygon], _gt: list[Polygon]) -> float:
+    intersect = 0
+    union = 0
 
-    cubes = []
-    for i in range(len(_tr)-1):
-        if not polygons[i + 1] or not polygons[i]:
+    for t, g in zip(_tr, _gt):
+        if (t is None or t.area == 0.0) and (g is None or g.area == 0.0):
             continue
-        makarena = [ConvexPolygon(polygons[i])]
-        makarena.append(ConvexPolygon((polygons[i][0], polygons[i][1], polygons[i+1][1], polygons[i+1][0])))
-        makarena.append(ConvexPolygon((polygons[i][1], polygons[i+1][1], polygons[i+1][2], polygons[i][2])))
-        makarena.append(ConvexPolygon((polygons[i][2], polygons[i+1][2], polygons[i+1][3], polygons[i][3])))
-        makarena.append(ConvexPolygon((polygons[i][3], polygons[i+1][3], polygons[i+1][0], polygons[i][0])))
-        makarena.append(ConvexPolygon(polygons[i+1]))
-        cubes.append(ConvexPolyhedron(makarena))
+        elif t is None or t.area == 0.0:
+            union += g.area
+        elif g is None or g.area == 0.0:
+            union += t.area
+        else:
+            intersect += t.intersection(g).area
+            union += t.union(g).area
 
-    return cubes
-
-
-def stt_iou(_tr: list[Polygon], _gt: list[Polygon]):
-    tr_polygons = [polygon_to_intarray(polygon) if polygon is not None else [] for polygon in _tr]
-    gt_polygons = [polygon_to_intarray(polygon) if polygon is not None else [] for polygon in _gt]
-
-    tr_cubes = create_cubes(tr_polygons)
-    gt_cubes = create_cubes(gt_polygons)
-
-    tr_volume = 0
-    gt_volume = 0
-    intersection_volume = 0
-
-    for tr_cube, gt_cube in zip(tr_cubes, gt_cubes):
-        tr_volume += tr_cube.volume()
-        gt_volume += gt_cube.volume()
-        _intersection = intersection(tr_cube, gt_cube)
-        if _intersection is None or type(_intersection) is not ConvexPolyhedron:
-            continue
-        intersection_volume += _intersection.volume()
-
-    if tr_volume + gt_volume - intersection_volume == 0:
-        return 0
-
-    return intersection_volume / (tr_volume + gt_volume - intersection_volume)
+    return intersect / union
 
 
 def average_stt_iou(
@@ -89,23 +58,9 @@ if __name__ == '__main__':
     results.trajectory = [[create_polygon(points) if points != [] else None for points in trajectory] for trajectory in trajectories]
     results.groundtruth = [[create_polygon(points) if points != [] else None for points in groundtruth] for groundtruth in groundtruths]
 
-    df = results.loc[(results['tracker'] == 'MedianFlow') & (results['dataset'] == 'VOT Basic Test Stack') & (results['sequence'] == 'sunshade'), ['trajectory', 'groundtruth']]
+    df = results.loc[(results['tracker'] == 'TLD') & (results['dataset'] == 'VOT Basic Test Stack') & (results['sequence'] == 'surfing'), ['trajectory', 'groundtruth']]
 
     print(stt_iou(
         df['trajectory'].tolist()[0],
         df['groundtruth'].tolist()[0]
     ))
-
-    r = Renderer()
-
-    tr_cubes = create_cubes([polygon_to_intarray(polygon) if polygon is not None else [] for polygon in df['trajectory'].tolist()[0]])
-    gt_cubes = create_cubes([polygon_to_intarray(polygon) if polygon is not None else [] for polygon in df['groundtruth'].tolist()[0]])
-    for tr_cube, gt_cube in zip(tr_cubes, gt_cubes):
-        r.add((tr_cube, 'r', 1))
-        r.add((gt_cube, 'g', 1))
-        cph2=intersection(tr_cube, gt_cube)
-        if cph2 is None or type(cph2) is not ConvexPolyhedron:
-            continue
-        r.add((cph2, 'b', 1))
-
-    r.show()
---
+++ analysis/visualize_stt_iou.py
@@ -0,0 +1,56 @@
+import json
+
+import pandas
+from Geometry3D import Point, ConvexPolygon, ConvexPolyhedron, Renderer, intersection
+
+from analysis.utils import polygon_to_intarray, create_polygon
+from stack import results_file
+
+
+def create_cubes(_tr: list[list[int]]):
+    polygons = []
+    for z, t in enumerate(_tr):
+        points = []
+        for x,y in zip(t[:-2:2], t[1:-2:2]):
+            points.append(Point(x,y,z))
+        polygons.append(points)
+
+    cubes = []
+    for i in range(len(_tr)-1):
+        if not polygons[i + 1] or not polygons[i]:
+            continue
+        makarena = [ConvexPolygon(polygons[i])]
+        makarena.append(ConvexPolygon((polygons[i][0], polygons[i][1], polygons[i+1][1], polygons[i+1][0])))
+        makarena.append(ConvexPolygon((polygons[i][1], polygons[i+1][1], polygons[i+1][2], polygons[i][2])))
+        makarena.append(ConvexPolygon((polygons[i][2], polygons[i+1][2], polygons[i+1][3], polygons[i][3])))
+        makarena.append(ConvexPolygon((polygons[i][3], polygons[i+1][3], polygons[i+1][0], polygons[i][0])))
+        makarena.append(ConvexPolygon(polygons[i+1]))
+        cubes.append(ConvexPolyhedron(makarena))
+
+    return cubes
+
+
+if __name__ == '__main__':
+    results = pandas.read_csv(f"../{results_file}")
+    trajectories = results.trajectory.apply(json.loads)
+    groundtruths = results.groundtruth.apply(json.loads)
+    results.trajectory = [[create_polygon(points) if points != [] else None for points in trajectory] for trajectory in trajectories]
+    results.groundtruth = [[create_polygon(points) if points != [] else None for points in groundtruth] for groundtruth in groundtruths]
+
+    df = results.loc[(results['tracker'] == 'TLD') & (results['dataset'] == 'VOT Basic Test Stack') & (results['sequence'] == 'surfing'), ['trajectory', 'groundtruth']]
+
+    r = Renderer()
+
+    tr_cubes = create_cubes([polygon_to_intarray(polygon) if polygon is not None else [] for polygon in df['trajectory'].tolist()[0]])
+    gt_cubes = create_cubes([polygon_to_intarray(polygon) if polygon is not None else [] for polygon in df['groundtruth'].tolist()[0]])
+    for tr_cube, gt_cube in zip(tr_cubes, gt_cubes):
+        r.add((tr_cube, 'r', 1))
+        # r.add((gt_cube, 'g', 1))
+        cph2=intersection(tr_cube, gt_cube)
+        if cph2 is None or type(cph2) is not ConvexPolyhedron:
+            continue
+        r.add((cph2, 'b', 1))
+
+    # 25/-80
+    # 5/-85
+    r.show() # linie 28 i 29
