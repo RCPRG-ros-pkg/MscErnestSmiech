8c71ebeec981bed1a96910d6a18e97472cd3210b
box_of_the_fox <ernest.smiech@wp.pl>
2024-02-28 17:50:39 +0100

    extracted calculations to viewmodel

--- data/viewmodel_errors.py
+++ data/viewmodel_errors.py
@@ -1,20 +1,39 @@
+import glob
+
+import numpy
 import pandas
 
+import streamlit as st
+
+from analysis.accuracy import success_plot, sequence_accuracy
+from analysis.longterm import quality_auxiliary, count_frames
+from analysis.stt_iou import stt_iou
+from analysis.time import sequence_time, time_quality_auxiliary
 from data.data_locator import DataLocator
 from data.singleton_meta import SingletonMeta
+from data.state_locator import StateLocator
+from stack import datasets
 
 
 class ErrorsViewModel(metaclass=SingletonMeta):
     _data_locator = DataLocator()
+    _state_locator = StateLocator()
 
     _images: [str] = []
-    _tracker_selection = {
-        'tracker': [],
-        'dataset': [],
-        'sequence': [],
-        'date': [],
-    }
     _selected_rows: None | pandas.DataFrame = None
+
+    def __init__(self):
+        super().__init__()
+
+        self._tracker_selection = {
+            'tracker': self.df['tracker'].unique().tolist(),
+            'dataset': self.df['dataset'].unique().tolist(),
+            'sequence': self.df['sequence'].unique().tolist(),
+            'date': self.df['date'].unique().tolist(),
+        }
+
+    def get_image_count(self) -> int:
+        return self._state_locator.provide_image_count()
 
     def set_selected_rows(self, df: pandas.DataFrame):
         self._selected_rows = df
@@ -31,8 +50,25 @@ class ErrorsViewModel(metaclass=SingletonMeta):
     def set_images(self, images: [str]):
         self._images = images
 
-    def set_tracker_selection(self, d: dict[str, list]):
-        self._tracker_selection = d
+    def load_images(self):
+        d = self.get_selected_rows().head(1)
+        d = d[['tracker', 'dataset', 'sequence', 'date']].astype(str).values.flatten().tolist()
+        d[1] = datasets[d[1]]
+        self.set_images(glob.glob(f"./raw/errors/{'/'.join(d)}/*"))
+
+    def set_tracker_selection(
+            self,
+            trackers: numpy.ndarray,
+            datasets: numpy.ndarray,
+            sequences: numpy.ndarray,
+            dates: numpy.ndarray
+    ):
+        self._tracker_selection = {
+            'tracker': trackers if trackers else self.df['tracker'].unique().tolist(),
+            'dataset': datasets if datasets else self.df['dataset'].unique().tolist(),
+            'sequence': sequences if sequences else self.df['sequence'].unique().tolist(),
+            'date': dates if dates else self.df['date'].unique().tolist(),
+        }
 
     def get_df_table(self):
         return self.df[
@@ -45,3 +81,104 @@ class ErrorsViewModel(metaclass=SingletonMeta):
     @property
     def df(self) -> pandas.DataFrame:
         return self._data_locator.provide_results()[['tracker', 'dataset', 'sequence', 'date', 'selected']]
+
+    def on_table_change(self, df_table):
+        if 'table' in st.session_state:
+            edited = st.session_state.table['edited_rows']
+
+            selected_indexes = []
+            for i in edited:
+                if edited[i]['selected']:
+                    selected_indexes.append(i)
+
+            self.set_selected_rows(df_table.iloc[selected_indexes])
+
+    def increase_image_count(self):
+        self._state_locator.increase_image_count()
+
+    def decrease_image_count(self):
+        self._state_locator.decrease_image_count()
+
+    def zero_image_count(self):
+        self._state_locator.zero_image_count()
+
+    def get_success_plot(self) -> None | pandas.DataFrame:
+        selected_rows = self.get_selected_rows()
+        if selected_rows is None:
+            return None
+
+        _df = self._data_locator.provide_results()[self._data_locator.provide_results()['date'].isin(selected_rows['date'])]
+
+        success = pandas.DataFrame(map(success_plot, _df['trajectory'], _df['groundtruth']), index=_df['date']) \
+            .unstack() \
+            .reset_index()[['date', 0]] \
+            .sort_values(by=['date', 0]) \
+            .reset_index(drop=True)
+
+        arr = numpy.array([item for row in success[0] for item in row])
+        arr = arr.reshape((len(arr)//2, 2))
+        tmp = pandas.DataFrame(arr, columns=['threshold', 'success'])
+        tmp['date'] = success['date']
+
+        return tmp
+
+    def get_time_tables(self) -> None | pandas.DataFrame:
+        selected_rows = self.get_selected_rows()
+        if selected_rows is None:
+            return None
+
+        _df = self._data_locator.provide_results()[self._data_locator.provide_results()['date'].isin(selected_rows['date'])]
+
+        times_quality_auxiliary = pandas.DataFrame(
+            numpy.vectorize(time_quality_auxiliary)(_df['times'], _df['trajectory'], _df['groundtruth'])
+        ).T
+        times_sequence = pandas.DataFrame(
+            numpy.vectorize(sequence_time)(_df['times'], _df['trajectory'], _df['groundtruth']),
+            columns=["success"]
+        )
+
+        return pandas.concat(
+            [
+                _df[['tracker', 'dataset', 'sequence', 'date']],
+                times_quality_auxiliary.rename(columns={0: 'robustness', 1: "nre", 2: "dre", 3: "ad"}),
+                times_sequence,
+            ],
+            axis=1,
+        )
+
+    def get_iou_table(self) -> None | pandas.DataFrame:
+        selected_rows = self.get_selected_rows()
+        if selected_rows is None:
+            return None
+
+        _df = self._data_locator.provide_results()[self._data_locator.provide_results()['date'].isin(selected_rows['date'])]
+
+        quality = pandas.DataFrame(
+            numpy.vectorize(quality_auxiliary)(_df['trajectory'], _df['groundtruth'])
+        ).T
+        accuracy = pandas.DataFrame(
+            numpy.vectorize(sequence_accuracy)(_df['trajectory'], _df['groundtruth']),
+            columns=("accuracy",)
+        )
+        stt_ious = pandas.DataFrame(
+            numpy.vectorize(stt_iou)(_df['trajectory'], _df['groundtruth']),
+            columns=("stt_iou",)
+        )
+
+        T, F, M, _, _ = numpy.vectorize(count_frames)(_df['trajectory'], _df['groundtruth'])
+        robustness = pandas.DataFrame(T / (T + F + M), columns=['robustness'])
+
+        return pandas.concat(
+            [
+                _df[['tracker', 'dataset', 'sequence', 'date']],
+                quality.rename(columns={0: "nre", 1: "dre", 2: "ad"}),
+                accuracy,
+                stt_ious,
+                robustness,
+            ],
+            axis=1,
+        ).rename(columns={0: "success"})
+
+    def get_ar_plot(self):
+        return self.get_iou_table()[['accuracy', 'robustness', 'date']]
+
--- pages/1_errors.py
+++ pages/1_errors.py
@@ -1,26 +1,14 @@
-import glob
-
-import numpy
-import pandas
 import streamlit as st
 from streamlit.delta_generator import DeltaGenerator
 
-from analysis.stt_iou import stt_iou
-from analysis.accuracy import sequence_accuracy, success_plot
-from analysis.longterm import quality_auxiliary, count_frames
-from analysis.time import time_quality_auxiliary, sequence_time
-from data.data_locator import DataLocator
-from data.state_locator import StateLocator
 from data.viewmodel_errors import ErrorsViewModel
-from stack import datasets
 
 
+# todo przycisk clear
+# todo wybór przedziału czasu
 class ErrorsPage:
     errors_view_model = ErrorsViewModel()
-    state_locator = StateLocator()
-    data_locator = DataLocator()
-    
-    df = errors_view_model.df
+
     image_displayer: DeltaGenerator | None = None
 
     def __init__(self) -> None:
@@ -37,118 +25,15 @@ class ErrorsPage:
             if len(selected_rows) == 1:
                 self.display_picture()
 
-            _df = self.data_locator.provide_results()[self.data_locator.provide_results()['date'].isin(selected_rows['date'])]
-            if not _df.empty:
-                quality = pandas.DataFrame(
-                    numpy.vectorize(quality_auxiliary)(_df['trajectory'], _df['groundtruth'])
-                ).T
-                accuracy = pandas.DataFrame(
-                    numpy.vectorize(sequence_accuracy)(_df['trajectory'], _df['groundtruth']),
-                    columns=("accuracy",)
-                )
-                stt_ious = pandas.DataFrame(
-                    numpy.vectorize(stt_iou)(_df['trajectory'], _df['groundtruth']),
-                    columns=("stt_iou",)
-                )
+            st.markdown("## Scores")
 
-                T, F, M, _, _ = numpy.vectorize(count_frames)(_df['trajectory'], _df['groundtruth'])
-                robustness = pandas.DataFrame(T / (T + F + M), columns=['robustness'])
+            self.display_plots()
+            self.display_iou_table()
+            self.display_time_table()
 
-                success = pandas.DataFrame(map(success_plot, _df['trajectory'], _df['groundtruth']), index=_df['date']) \
-                    .unstack() \
-                    .reset_index()[['date', 0]] \
-                    .sort_values(by=['date', 0]) \
-                    .reset_index(drop=True)
+        self.display_selector()
 
-                arr = numpy.array([item for row in success[0] for item in row])
-                arr = arr.reshape((len(arr)//2, 2))
-                tmp = pandas.DataFrame(arr, columns=['threshold', 'success'])
-                tmp['date'] = success['date']
-                success = tmp
-
-                times_quality_auxiliary = pandas.DataFrame(
-                    numpy.vectorize(time_quality_auxiliary)(_df['times'], _df['trajectory'], _df['groundtruth'])
-                ).T
-                times_sequence = pandas.DataFrame(
-                    numpy.vectorize(sequence_time)(_df['times'], _df['trajectory'], _df['groundtruth']),
-                    columns=["success"]
-                )
-
-                times = pandas.concat(
-                    [
-                        _df[['tracker', 'dataset', 'sequence', 'date']],
-                        times_quality_auxiliary.rename(columns={0: 'robustness', 1: "nre", 2: "dre", 3: "ad"}),
-                        times_sequence,
-                    ],
-                    axis=1,
-                )
-
-                _df = pandas.concat(
-                    [
-                        _df[['tracker', 'dataset', 'sequence', 'date']],
-                        quality.rename(columns={0: "nre", 1: "dre", 2: "ad"}),
-                        accuracy,
-                        stt_ious,
-                        robustness,
-                    ],
-                    axis=1,
-                ).rename(columns={0: "success"})
-
-                st.markdown("## Scores")
-
-                iou_quality, iou_ar = st.columns(2)
-
-                with iou_quality:
-                    st.text("Quality plot")
-                    st.line_chart(success, x='threshold', y='success', color='date', height=300)
-
-                with iou_ar:
-                    st.text("AR plot")
-                    st.scatter_chart(_df[['accuracy', 'robustness', 'date']], x='robustness', y='accuracy', color='date', height=300)
-
-                st.text("Iou table")
-                st.data_editor(
-                    _df,
-                    column_config={
-                        "tracker": "Tracker",
-                        "dataset": "Dataset",
-                        "sequence": "Sequence",
-                        "date": "Date",
-                        "accuracy": "Accuracy",
-                        "robustness": "Robustness",
-                        "stt_iou": "STT-IoU",
-                        "nre": "NRE",
-                        "dre": "DRE",
-                        "ad": "AD"
-                    },
-                    disabled=['tracker', "dataset", "robustness", 'nre', 'dre', 'ad', 'sequence', 'accuracy', 'date', 'stt_iou'],
-                    use_container_width=True,
-                    key='foo',
-                    column_order=["tracker", "dataset", "sequence", 'date', "accuracy", "robustness", "stt_iou", 'nre', 'dre', 'ad'],
-                    hide_index=True
-                )
-
-                st.text("Times table")
-                st.data_editor(
-                    times,
-                    column_config={
-                        "tracker": "Tracker",
-                        "dataset": "Dataset",
-                        "sequence": "Sequence",
-                        "date": "Date",
-                        "success": "Success",
-                        "robustness": "Robustness",
-                        "nre": "NRE",
-                        "dre": "DRE",
-                        "ad": "AD"
-                    },
-                    disabled=['tracker', "dataset", 'sequence', "robustness", 'nre', 'dre', 'ad', 'success', 'date'],
-                    use_container_width=True,
-                    key='bar',
-                    column_order=["tracker", "dataset", "sequence", 'date', "success", "robustness", 'nre', 'dre', 'ad'],
-                    hide_index=True
-                )
-
+    def display_selector(self):
         st.markdown("## Selector")
         st.data_editor(
             self.errors_view_model.get_df_table(),
@@ -164,7 +49,7 @@ class ErrorsPage:
                 ),
             },
             disabled=["tracker", "dataset", "sequence", "date"],
-            on_change=self.on_table_change,
+            on_change=self.errors_view_model.on_table_change,
             use_container_width=True,
             key='table',
             column_order=["tracker", "dataset", "sequence", "date", "selected"],
@@ -172,16 +57,94 @@ class ErrorsPage:
             args=[self.errors_view_model.get_df_table()]
         )
 
+    def display_time_table(self):
+        data = self.errors_view_model.get_time_tables()
+        if data is None or data.empty:
+            return
+
+        st.text("Times table")
+        st.data_editor(
+            data,
+            column_config={
+                "tracker": "Tracker",
+                "dataset": "Dataset",
+                "sequence": "Sequence",
+                "date": "Date",
+                "success": "Success",
+                "robustness": "Robustness",
+                "nre": "NRE",
+                "dre": "DRE",
+                "ad": "AD"
+            },
+            disabled=['tracker', "dataset", 'sequence', "robustness", 'nre', 'dre', 'ad', 'success', 'date'],
+            use_container_width=True,
+            key='bar',
+            column_order=["tracker", "dataset", "sequence", 'date', "success", "robustness", 'nre', 'dre', 'ad'],
+            hide_index=True
+        )
+
+    def display_iou_table(self):
+        data = self.errors_view_model.get_iou_table()
+        if data is None or data.empty:
+            return
+
+        st.text("Iou table")
+        st.data_editor(
+            data,
+            column_config={
+                "tracker": "Tracker",
+                "dataset": "Dataset",
+                "sequence": "Sequence",
+                "date": "Date",
+                "accuracy": "Accuracy",
+                "robustness": "Robustness",
+                "stt_iou": "STT-IoU",
+                "nre": "NRE",
+                "dre": "DRE",
+                "ad": "AD"
+            },
+            disabled=['tracker', "dataset", "robustness", 'nre', 'dre', 'ad', 'sequence', 'accuracy', 'date',
+                      'stt_iou'],
+            use_container_width=True,
+            key='foo',
+            column_order=["tracker", "dataset", "sequence", 'date', "accuracy", "robustness", "stt_iou", 'nre', 'dre',
+                          'ad'],
+            hide_index=True
+        )
+
+    def display_plots(self):
+        data = self.errors_view_model.get_success_plot()
+        if data is None or data.empty:
+            return
+
+        iou_quality, iou_ar = st.columns(2)
+        with iou_quality:
+            st.text("Quality plot")
+            st.line_chart(
+                data,
+                x='threshold',
+                y='success',
+                color='date',
+                height=300
+            )
+        with iou_ar:
+            st.text("AR plot")
+            st.scatter_chart(
+                self.errors_view_model.get_ar_plot(),
+                x='robustness',
+                y='accuracy',
+                color='date',
+                height=300
+            )
+
     def display_picture(self):
-        d = self.errors_view_model.get_selected_rows().head(1)
-        d = d[['tracker', 'dataset', 'sequence', 'date']].astype(str).values.flatten().tolist()
-        d[1] = datasets[d[1]]
-        self.errors_view_model.set_images(glob.glob(f"./raw/errors/{'/'.join(d)}/*"))
+        self.errors_view_model.load_images()
+
         try:
-            image = self.errors_view_model.get_image(self.state_locator.provide_image_count())
+            image = self.errors_view_model.get_image(self.errors_view_model.get_image_count())
         except IndexError:
-            self.state_locator.zero_image_count()
-            image = self.errors_view_model.get_image(self.state_locator.provide_image_count())
+            self.errors_view_model.zero_image_count()
+            image = self.errors_view_model.get_image(self.errors_view_model.get_image_count())
 
         st.markdown("## Errors")
 
@@ -191,11 +154,11 @@ class ErrorsPage:
 
             _, col1, col2, col3, _ = st.columns((3, 1, 1, 1, 3))
             with col1:
-                st.button("←", on_click=self.state_locator.decrease_image_count)
+                st.button("←", on_click=self.errors_view_model.decrease_image_count)
             with col2:
-                st.write(f"{self.state_locator.provide_image_count()}/{len(self.errors_view_model.get_images())}")
+                st.write(f"{self.errors_view_model.get_image_count()}/{len(self.errors_view_model.get_images())}")
             with col3:
-                st.button("→", on_click=self.state_locator.increase_image_count)
+                st.button("→", on_click=self.errors_view_model.increase_image_count)
 
     def sidebar(self) -> None:
         with st.sidebar.form("Options"):
@@ -205,25 +168,7 @@ class ErrorsPage:
             dates = st.multiselect("Date", self.errors_view_model.df['date'].unique())
 
             if st.form_submit_button("Submit", use_container_width=True, type="primary"):
-                self.errors_view_model.set_tracker_selection(
-                    {
-                        'tracker': trackers if trackers else self.errors_view_model.df['tracker'].unique().tolist(),
-                        'dataset': datasets if datasets else self.errors_view_model.df['dataset'].unique().tolist(),
-                        'sequence': sequences if sequences else self.errors_view_model.df['sequence'].unique().tolist(),
-                        'date': dates if dates else self.errors_view_model.df['date'].unique().tolist(),
-                    }
-                )
-
-    def on_table_change(self, df_table):
-        if 'table' in st.session_state:
-            edited = st.session_state.table['edited_rows']
-
-            selected_indexes = []
-            for i in edited:
-                if edited[i]['selected']:
-                    selected_indexes.append(i)
-
-            self.errors_view_model.set_selected_rows(df_table.iloc[selected_indexes])
+                self.errors_view_model.set_tracker_selection(trackers, datasets, sequences, dates)
 
 
 ErrorsPage()
