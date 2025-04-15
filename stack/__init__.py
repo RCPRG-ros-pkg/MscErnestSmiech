742c41a6a6fb92b31a9c1a68b073caed1d0f5003
BoxOfTheFox <51001597+BoxOfTheFox@users.noreply.github.com>
2025-04-15 22:53:42 +0200

    Add long term testing dataset (#9)
    Added long term dataset

--- stack/__init__.py
+++ stack/__init__.py
@@ -1,7 +1,7 @@
 datasets = {
     'VOT Basic Test Stack': 'basic',
-    'VOT-ST2022 bounding-box challenge': 'vot2022stb',
-    'VOT-ST2018 long term challenge': 'vot2018lt',
+    'VOT-ST2022 short term': 'vot2022stb',
+    'VOT-ST2022 long term': 'vot2022lt',
 }
 results_dir = './raw/results'
 cache_dir = './raw/caches'
